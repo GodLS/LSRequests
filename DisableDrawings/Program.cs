e4bbb73266bf89e3c78c93bf91645cfde0f50f2d
GodLS <lmyrunner2@gmail.com>
2015-07-30 13:07:34 -0400

    needs a toggle key and shit to be useful tho

---
+++ DisableDrawings/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace DisableDrawings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hacks.DisableDrawings = true;
+        }
+    }
+}
